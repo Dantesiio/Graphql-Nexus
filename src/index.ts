3c6af05f047eec4a2b1382d46692e316ca6cbc77
tyronemab <jhonatancastanoibarra@gmail.com>
2025-06-09 22:16:15 -0500

    playground for production true

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { ApolloServer } from 'apollo-server';
 import { schema } from './graphql/schema';
 import { createContext } from './auth/middleware';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
 const server = new ApolloServer({
   schema,
   context: createContext,
+  introspection: true, // permite introspección en producción
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
 server.listen().then(({ url }) => {
